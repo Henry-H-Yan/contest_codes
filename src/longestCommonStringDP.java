ba0d219a52d8d8f2b1e45d8b762554bbf3701317
Henry-H-Yan <henryyan829@gmail.com>
2015-04-08 03:20:13 -0400

    practice dynamic programming

---
+++ src/longestCommonStringDP.java
@@ -0,0 +1,37 @@
+public class longestCommonStringDP {
+
+    public static void main(String[] args) {
+        String x = "1122334455";
+        String y ="12345";
+        int M = x.length();
+        int N = y.length();
+
+        int[][] opt = new int[M+1][N+1];
+
+        // compute length of LCS and all subproblems via dynamic programming
+        for (int i = M-1; i >= 0; i--) {
+            for (int j = N-1; j >= 0; j--) {
+                if (x.charAt(i) == y.charAt(j))
+                    opt[i][j] = opt[i+1][j+1] + 1;
+                else 
+                    opt[i][j] = Math.max(opt[i+1][j], opt[i][j+1]);
+            }
+        }
+        System.out.println("LCS length is "+ opt[0][0]);
+
+        // recover LCS itself and print it to standard output
+        int i = 0, j = 0;
+        while(i < M && j < N) {
+            if (x.charAt(i) == y.charAt(j)) {
+                System.out.print(x.charAt(i));
+                i++;
+                j++;
+            }
+            else if (opt[i+1][j] >= opt[i][j+1]) i++;
+            else                                 j++;
+        }
+        System.out.println();
+
+    }
+
+}
