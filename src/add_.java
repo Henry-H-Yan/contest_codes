1e2ddc39257227238da91784029f38992dbbbfb2
Henry-H-Yan <henryyan829@gmail.com>
2015-04-15 00:33:04 -0400

    clean up

--- src/add_.java
+++ src/add_.java
@@ -20,6 +20,7 @@ public class add_ {
 		// written for a cacheism assignment for computer architechture class, translate to machine bits
 	int [] list={2,6, 9, 3, 20, 98, 19, 56, 8, 11,6, 43, 5,3, 9, 98};
 	int [] list2={3,180,43,2,191,88,190,14,181,44,4,100,180,186,100,4,253};
+	int[] list3 ={ 181, 44, 3, 192, 89, 191, 15, 182, 45, 181,186, 253,43,44,52,13,68, 101,52};
 //		for(int i:list)System.out.println ( 
 //				to32 (  Integer.toString(i,2) )
 //				);
@@ -38,6 +39,12 @@ public class add_ {
 				);
 		System.out.println("second part of instructions");
 		for(int i:list2)System.out.println ( 
+				to32 (  Integer.toString(i,2) ) .substring(0,20)+"||" + 
+						to32 (  Integer.toString(i,2) ) .substring(20,30)+ "|| "
+				+to32 (  Integer.toString(i,2) ) .substring(30,32)
+				);
+		System.out.println("3rd  part of instructions");
+		for(int i:list3)System.out.println ( 
 				to32 (  Integer.toString(i,2) ) .substring(0,20)+"||" + 
 						to32 (  Integer.toString(i,2) ) .substring(20,30)+ "|| "
 				+to32 (  Integer.toString(i,2) ) .substring(30,32)
